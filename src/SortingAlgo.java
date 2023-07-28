6eecbe6b28443858484524415906365e7681f4de
chaitanyapadalkar <chaitanyapadalkar18@gmail.com>
2023-07-28 14:41:54 +0530

    Buubles Sort file

--- src/SortingAlgo.java
+++ src/SortingAlgo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BubbleSort {
+public class SortingAlgo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -9,12 +9,25 @@ public class BubbleSort {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-
-        bubble_sort(arr);
-
+        selection_sort(arr);
+//        bubble_sort(arr);
+//        insertion_sort(arr);
         for (int i:arr
              ) {
             System.out.print(i+" ");
+        }
+    }
+
+    private static void insertion_sort(int[] arr) {
+
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j >0 ; j--) {
+                if(arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+                }
+                else
+                    break;
+            }
         }
     }
 
@@ -36,6 +49,26 @@ public class BubbleSort {
             // if you did not swap for a particular value of i, it means the array is sorted hence stop the program
             if (!swapped) { // !false = true
                 break;
+            }
+        }
+    }
+
+    static void selection_sort(int[] arr){
+
+        for(int i = 0; i< arr.length;i++){
+
+            int minIndex=i;
+            for(int j=i+1; j< arr.length;j++){
+                if(arr[j]<arr[minIndex]){
+                    minIndex=j;
+                }
+            }
+
+            if(minIndex!=i){
+                int temp;
+                temp=arr[i];
+                arr[i]=arr[minIndex];
+                arr[minIndex]=temp;
             }
         }
     }
